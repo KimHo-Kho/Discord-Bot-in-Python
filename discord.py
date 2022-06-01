8838ac4cc92a74106d94c5de9b9ba22dd2790721
Kim Ho <58868324+KimHo-Kho@users.noreply.github.com>
2022-06-01 15:58:17 -0700

    Update discord.py

--- discord.py
+++ discord.py
@@ -11,6 +11,13 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
 
 client.run(TOKEN)
